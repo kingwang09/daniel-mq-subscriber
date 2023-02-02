5f48096789bf2c362f6f586361973ffe6cc8c288
hyungeun <kingwang09@gmail.com>
2023-02-02 09:35:09 +0900

    2개의 exchange에 대해 수신 로직 추가

--- src/main/java/org/daniel/mq/subscriber/hello/listener/HelloQueueReceiver.java
+++ src/main/java/org/daniel/mq/subscriber/hello/listener/HelloQueueReceiver.java
@@ -2,6 +2,9 @@ package org.daniel.mq.subscriber.hello.listener;
 
 import lombok.extern.slf4j.Slf4j;
 import org.daniel.mq.subscriber.hello.entity.Message;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +12,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloQueueReceiver {
 
-    @RabbitListener(queues="hello.queue")
-    public void consume(final Message message){
-        log.debug("hello queue receive: {}", message);
+    @RabbitListener(bindings = {@QueueBinding(value = @Queue(name = "hello.queue"), exchange = @Exchange(name="world.exchange"))})
+    public void wordExchangeConsume(final Message message){
+        log.debug("hello queue > world.exchange receive: {}", message);
+    }
+
+    @RabbitListener(bindings = {@QueueBinding(value = @Queue(name = "hello.queue"), exchange = @Exchange(name="rabbit.exchange"))})
+    public void rabbitMQExchangeConsume(final Message message){
+        log.debug("hello queue > rabbit.exchange receive: {}", message);
     }
 }
