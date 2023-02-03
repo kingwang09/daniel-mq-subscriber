e1cf8515c5cffec92c9f5388c1f9880c676acb55
hyungeun <kingwang09@gmail.com>
2023-02-03 12:14:28 +0900

    subscriber localDateTime 받도록 처리

--- src/main/java/org/daniel/mq/subscriber/hello/entity/Message.java
+++ src/main/java/org/daniel/mq/subscriber/hello/entity/Message.java
@@ -1,11 +1,15 @@
 package org.daniel.mq.subscriber.hello.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -14,4 +18,8 @@ import java.io.Serializable;
 public class Message implements Serializable {
     private String message;
     private String name;
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    private LocalDateTime registeredAt;
 }
